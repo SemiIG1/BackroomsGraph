490c7df470b2d76a76506e93190cabe701cc0d60
SemiIG1 <fauzan.agusta01@gmail.com>
2026-07-21 08:54:43 +0700

    Added closest route mode

--- src/main/java/com/fauzan/backrooms/restcontroller/LevelRestController.java
+++ src/main/java/com/fauzan/backrooms/restcontroller/LevelRestController.java
@@ -2,10 +2,16 @@ package com.fauzan.backrooms.restcontroller;
 
 import com.fauzan.backrooms.dto.GraphResponse;
 import com.fauzan.backrooms.service.LevelService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin(origins = "*")
 public class LevelRestController {
     private final LevelService levelService;
 
@@ -15,5 +21,12 @@ public class LevelRestController {
     @GetMapping("/api/graph")
     public GraphResponse getAllNodesAndEdges() {
         return levelService.getAllNodesAndEdges();
+    }
+
+    @GetMapping("/api/route")
+    public List<String> getShortestRoute(
+            @RequestParam String start,
+            @RequestParam String end) {
+        return levelService.getClosestRoute(start, end);
     }
 }
